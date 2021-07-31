7a26c5b569a864ce01de3ea8053d32ec0b7c1253
Erick Andres <eandres1@berkeley.edu>
2021-07-31 00:55:40 +0000

    added coverage

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -16,6 +16,7 @@ if RUBY_VERSION>='2.6.0'
 end
 
 describe MoviesController do
+  
   describe '#show_same_director' do
     it 'has a director' do 
       same_director_movies = ['Django', 'Tarantino']
@@ -32,7 +33,59 @@ describe MoviesController do
       allow(Movie).to receive(:with_director).with('')
      .and_return(same_director_movies)
       get :show_same_director, {id: 2, show_same_director: same_director_movies}
+      expect(flash[:warning]).to match(/has no director info/)
       expect(response).to redirect_to(movies_path)
+    end
+  end
+  
+  describe '#show' do 
+    let!(:movie) {FactoryBot.create(:movie)}
+    it 'shows movies page' do 
+        get :show, id: movie.id
+        expect(response).to render_template(:show)
+    end
+  end
+  
+  describe '#index' do 
+    let!(:movie) {FactoryBot.create(:movie)}
+    it 'shows movies page' do 
+        get :index
+        expect(response).to render_template(:index)
+    end
+  end
+  
+  describe '#create' do 
+    let!(:movie) {FactoryBot.create(:movie)}
+    it 'creates movie and redirects to movies page' do 
+        post :create, movie: FactoryBot.attributes_for(:movie)
+        expect(response).to redirect_to(movies_url)
+    end
+  end
+  
+  describe '#edit' do 
+    let!(:movie) {FactoryBot.create(:movie)}
+    it 'edit movie in edit page' do 
+        get :edit, id: movie.id
+        expect(response).to render_template(:edit)
+    end
+  end
+  
+  describe '#update' do 
+    let!(:movie) {FactoryBot.create(:movie)}
+    let(:new_attributes) {
+      { title: "Superman1" }
+    }
+    it 'shows movies page' do 
+        put :update, id: movie.id, movie: new_attributes
+        expect(response).to redirect_to(movie_path(movie))
+    end
+  end
+  
+  describe '#destroy' do 
+    let!(:movie) {FactoryBot.create(:movie)}
+    it 'shows movies page' do 
+        delete :destroy, id: movie.id
+        expect(response).to redirect_to(movies_path)
     end
   end
 end
---
+++ rottenpotatoes/spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :movie do
+    title {'Spiderman'}
+    rating {'PG'}
+    description {'First Movie'}
+    release_date {'2002-05-17'}
+  end
+end
